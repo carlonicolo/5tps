55f08a7acd96719483ff4f9572ef1a6d525b9f13
Carlo <carlo.nicolo@hotmail.com>
2023-10-10 22:28:58 +0200

    Exceptions, finally word

--- Python/Polimorfismo/polimorfismo.py
+++ Python/Polimorfismo/polimorfismo.py
@@ -18,19 +18,19 @@ class Plane(Vehicle):
     print("Fly!")
 
 try:    
-    car1 = Car("Ford", "Mustang","PiPPO") #Create a Car object
-    boat1 = Boat("Ibiza", "Touring 20") #Create a Boat object
-    plane1 = Plane("Boeing", "747") #Create a Plane object
+  car1 = Car("Ford", "Mustang", "Pippo") #Create a Car object
+  boat1 = Boat("Ibiza", "Touring 20") #Create a Boat object
+  plane1 = Plane("Boeing", "747") #Create a Plane object
     
-    x = 10
-    if x < 0:
-        raise Exception("Sorry, no numbers below zero")
+    #x = 10
+    #if x < 0:
+        #raise Exception("Sorry, no numbers below zero")
 
-    for x in (car1, boat1, plane1):
-        print(x.brand)
-        print(x.model)
-        x.move()
+  for x in (car1, boat1, plane1):
+      print(x.brand)
+      print(x.model)
+      x.move()
 except Exception as err:
     print(f"Unexpected {err=}, {type(err)=}")
-    raise
-
+finally:
+  print("The 'try except' is finished")
