bbaba4633bc013889d042cfa8cfe9e9024476bb3
Carlo <carlo.nicolo@hotmail.com>
2023-11-09 23:32:18 +0100

    Fix

--- Python/InterfaceClass/interface_example.py
+++ Python/InterfaceClass/interface_example.py
@@ -6,14 +6,17 @@ class Bank(ABC):
    @abstractmethod
    def balance_check(self):
        pass
+   
    @abstractmethod
    def interest(self):
        pass
+
 class SBI(Bank):
    def balance_check(self):
        print("Balance is 100 rupees")
    def interest(self):
        print("SBI interest is 5 rupees")
+
 s = SBI()
 s.balance_check()
 s.interest()
