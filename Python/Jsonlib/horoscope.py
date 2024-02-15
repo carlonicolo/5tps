bfe5bca702524a4cf90cf904f942b53a2eba46af
Carlo <carlo.nicolo@hotmail.com>
2024-02-15 22:49:15 +0100

    Json

---
+++ Python/Jsonlib/horoscope.py
@@ -0,0 +1,29 @@
+import requests
+import json
+
+# pip install requests
+
+# Make the GET request to the horoscope API
+response = requests.get("https://horoscope-app-api.vercel.app/api/v1/get-horoscope/daily?sign=capricorn&day=today")
+data = response.json()  # Convert the response to JSON
+
+print(data)
+
+# Store the JSON data in a file
+with open("horoscope_data.json", "w") as file:
+    json.dump(data, file)
+
+print("Data stored successfully!")
+
+print(" ")
+
+# Retrieve JSON data from the file
+with open("horoscope_data.json", "r") as file:
+    data = json.load(file)
+
+# Access and process the retrieved JSON data
+date = data["data"]["date"]
+horoscope_data = data["data"]["horoscope_data"]
+
+# Print the retrieved data
+print(f"Horoscope for date {date}: {horoscope_data}")
