4a69abfbbc7ebeadf9eef38dc705cbbfdb760ccd
Carlo <carlo.nicolo@hotmail.com>
2024-09-25 22:26:25 +0200

    esercitazione Moduli

---
+++ 2024_25/Python/exModuli2/main.py
@@ -0,0 +1,19 @@
+import operazioni as op
+
+BMW_PER_DAY = 50
+AUDI_PER_DAY = 70
+
+def main():
+    
+    # Check se ha percorso più di 1000 km
+    # isMore = op.checkChilometraggio(kmpercorsi)
+    # if(isMore > 1000):..... else:....
+    pass
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ 2024_25/Python/exModuli2/operazioni.py
@@ -0,0 +1,9 @@
+# Funzioni
+
+def calcoloCostoGiornaliero(costo_giornaliero: int, giorni_noleggio: int) -> int:
+    pass
+
+
+
+def checkChilometraggio(chilometri_percorsi: int) -> bool:
+    pass
