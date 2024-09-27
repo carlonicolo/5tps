93a16aec12b737a9ff9c290557935ee9ccb3f59e
Carlo <carlo.nicolo@hotmail.com>
2024-09-27 23:50:49 +0200

    fix

--- 2024_25/Python/exModuli2/main.py
+++ 2024_25/Python/exModuli2/main.py
@@ -1,32 +1,62 @@
 import operazioni as op
+import re
+
 
 BMW_PER_DAY = 50
 AUDI_PER_DAY = 70
 
 def main():
-    
-    lista_auto = ["AUDI","BMW"]
-
-    for count, value in enumerate(lista_auto):
-        print(count, "-", value)
         
-    #Scelta delle operazioni
-    scelta_auto = int(input("Scegli quale auto noleggiare:   "))
-    print("\n##############################\n")
+    number = op.rentalOptions()
+    # print("Numero scelta: ", rental_choice)
     
-    if(scelta_auto == 0):
-        days = int(input("Quanti giorni hai noleggiato l'auto? "))
-        res = op.calcoloCostoGiornaliero(AUDI_PER_DAY, days)
+    flag = True
+
+    while(flag):
+        if re.match(r"^-?\d+$", number):
+            print("Here 2")
+            number = int(number)
+            print(f"The number you entered is {number}.")
+            flag = False
+        else:
+            print("Numero non corretto rinserisci il numero")
+            number = input()
+    
+    # 1 noleggio
+    if (number == 1):
         
+        lista_auto = ["AUDI","BMW"]
+        print("")
+        print("Scegli quale auto noleggiare")
+        
+        for count, value in enumerate(lista_auto):
+            print(count, "-", value)
+        scelta_auto = int(input("-> "))
+        
+        print("\n##############################\n")
+        
+        if(scelta_auto == 0):
+            days = int(input("Quanti giorni hai noleggiato l'auto? "))
+            res = op.calcoloCostoGiornaliero(AUDI_PER_DAY, days)
+            
+        if(scelta_auto == 1):
+            days = int(input("Quanti giorni hai noleggiato l'auto? "))
+            res = op.calcoloCostoGiornaliero(BMW_PER_DAY, days)
+        
+        print("Devi pagare: ", res, "€")
     
-    # Check se ha percorso più di 1000 km
-    # isMore = op.checkChilometraggio(kmpercorsi)
-    # if(isMore > 1000):..... else:....
-    
-
-
-
-
+        # 2 consegna
+        
+        ask = input("Inserisci intero")
+        num = op.checkIsDigit(ask)
+        
+        print(num)
+        
+        
+        
+        # Check se ha percorso più di 1000 km
+        # isMore = op.checkChilometraggio(kmpercorsi)
+        # if(isMore > 1000):..... else:....
 
 
 if __name__ == "__main__":
--- 2024_25/Python/exModuli2/operazioni.py
+++ 2024_25/Python/exModuli2/operazioni.py
@@ -1,4 +1,15 @@
+import re
+
 # Funzioni
+
+def rentalOptions() -> str:
+    print("Benvenuto in Predazzo Rental Auto")
+    print("Scegli tra le opzioni disponibili")
+    print("1 - Noleggio auto")
+    print("2 - Consegna auto")
+    choice = input()
+    return choice
+
 
 def calcoloCostoGiornaliero(costo_giornaliero: int, giorni_noleggio: int) -> int:
     return costo_giornaliero * giorni_noleggio
@@ -7,3 +18,19 @@ def calcoloCostoGiornaliero(costo_giornaliero: int, giorni_noleggio: int) -> int
 
 def checkChilometraggio(chilometri_percorsi: int) -> bool:
     pass
+
+
+def checkIsDigit(number: str):
+    flag = True
+
+    while(flag):
+        if re.match(r"^-?\d+$", number):
+            print("Here 2")
+            number = int(number)
+            print(f"The number you entered is {number}.")
+            flag = False
+        else:
+            print("Numero non corretto rinserisci il numero")
+            number = input()
+    
+    return number
