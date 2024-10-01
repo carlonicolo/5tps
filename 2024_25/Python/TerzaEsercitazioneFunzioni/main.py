72121302c99c13277ca13159590817b97af4d4ac
Carlo <carlo.nicolo@hotmail.com>
2024-10-01 21:50:42 +0200

    terza esercitazione - funzioni in python

---
+++ 2024_25/Python/TerzaEsercitazioneFunzioni/main.py
@@ -0,0 +1,16 @@
+'''
+Creare un programma che utilizza almeno:
+- una funzione di tipo void
+- una funzoine di tipo int
+- una funzione di tipo str
+- una funzione che restituisce due valore
+
+Le funzioni possono avere zero, uno o più parametri
+'''
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
