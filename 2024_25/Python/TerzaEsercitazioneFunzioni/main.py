df2bf08ca2672a98e9e633d935722e59753df767
Carlo <carlo.nicolo@hotmail.com>
2024-10-08 23:17:18 +0200

    esempio

--- 2024_25/Python/TerzaEsercitazioneFunzioni/main.py
+++ 2024_25/Python/TerzaEsercitazioneFunzioni/main.py
@@ -1,15 +1,19 @@
 '''
 Creare un programma che utilizza almeno:
 - una funzione di tipo void
-- una funzoine di tipo int
+- una funzione di tipo int
 - una funzione di tipo str
 - una funzione che restituisce due valore
 
 Le funzioni possono avere zero, uno o più parametri
 '''
+import utilities as u
+
 
 def main():
-    pass
+    res_somma, res_diff = u.computeAddSub(10,5)
+    print("Somma: ", res_somma)
+    print("Differenza: ", res_diff)
 
 
 if __name__ == "__main__":
--- 2024_25/Python/TerzaEsercitazioneFunzioni/utilities.py
+++ 2024_25/Python/TerzaEsercitazioneFunzioni/utilities.py
@@ -1,1 +1,12 @@
 # Funzioni utilità
+
+'''
+Funzione che ritorna due valori:
+sum somma dei due parametri
+sub differenza dei due parametri
+'''
+def computeAddSub(num1: int, num2: int) -> int:
+    sum = num1 + num2
+    sub = num1 - num2
+    return sum, sub
+
