0cd96e7f000ab0d709ad5a373e19e8271e01dc99
Carlo <carlo.nicolo@hotmail.com>
2024-11-06 23:04:31 +0100

    oggetti Persona e Studente

--- 2024_25/Java/CorsoJava/src/esercitazioneSottoclassi/Main.java
+++ 2024_25/Java/CorsoJava/src/esercitazioneSottoclassi/Main.java
@@ -2,4 +2,25 @@ package esercitazioneSottoclassi;
 
 
 public class Main {
+    public static void main(String[] args) {
+        Persona pippo = new Persona();
+        pippo.visualizza();
+
+        System.out.println();
+        Persona pluto = new Persona("Pluto", "Predazzo");
+        pluto.visualizza();
+
+        System.out.println();
+        Studente studente1 = new Studente("Black Jack", "Cavalese");
+        studente1.visualizza();
+
+        System.out.println();
+        System.out.println("Before");
+        System.out.println("Piano di studio: " + studente1.getPdS());
+        studente1.modificaPdS("INF");
+
+        System.out.println();
+        System.out.println("After");
+        System.out.println("Piano di studio: " + studente1.getPdS());
+    }
 }
