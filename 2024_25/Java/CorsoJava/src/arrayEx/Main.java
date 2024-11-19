04c269898ba8df50635ee592f8bb5dfc0667a507
Carlo <carlo.nicolo@hotmail.com>
2024-11-19 23:34:37 +0100

    Simple array ops

--- 2024_25/Java/CorsoJava/src/arrayEx/Main.java
+++ 2024_25/Java/CorsoJava/src/arrayEx/Main.java
@@ -2,6 +2,31 @@ package arrayEx;
 
 public class Main {
     public static void main(String[] args) {
+
+        /**
+         * Simple operation using array
+         */
+        int[] numbers = {2, -9, 0, 5, 12, -25, 22, 9, 8, 12};
+        int sum = 0;
+        double average;
+
+        // access all elements using for each loop
+        // add each element in sum
+        for (int number : numbers) {
+            sum += number;
+        }
+
+        // get the total number of elements
+        int arrayLength = numbers.length;
+
+        // calculate the average
+        // convert the average from int to double
+        average = ((double) sum / (double) arrayLength);
+
+        System.out.println("Sum = " + sum);
+        System.out.println("Average = " + average);
+
+
         Calculator calc = new Calculator();
         System.out.println(calc.getValues().length);
         System.out.println(calc.getValues()[0]);
